7a425265b6d1dc911f176c4fcc726ce5efd51331
stjohn <52610738+sequent5@users.noreply.github.com>
2020-11-17 09:20:59 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,1 @@
-cp  /opt/jar/config/v2proxy.conf /etc/nginx/conf.d/default.conf
-nohup java -jar -Xms40m -Xmx40m -XX:MaxDirectMemorySize=10M -XX:MaxMetaspaceSize=80m /opt/jar/v2ray-proxy.jar --spring.config.location=/opt/jar/config/proxy.yaml &
-nginx -g 'daemon off;'
+nohup java -jar -Xms40m -Xmx40m -XX:MaxDirectMemorySize=10M -XX:MaxMetaspaceSize=80m /opt/jar/v2ray-proxy.jar --spring.config.location=/opt/jar/config/proxy.yaml
